a486303a69b3969457912d5ac97c707c00586bba
gontijol <gontijol@live.com>
2023-10-06 15:53:35 -0300

    [NEW] Att colors

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-const lightBlue = Color(0xFF00B0FF);
-const darkBlue = Color(0xFF01579B);
-const lightGrey = Color(0xFFE0E0E0);
-const darkGrey = Color(0xFF212121);
-const white = Color(0xFFFFFFFF);
-const black = Color(0xFF000000);
-const red = Color(0xFFFF0000);
-const green = Color(0xFF00FF00);
-const yellow = Color(0xFFFFFF00);
+const mediumPurple = Color(0xFF8433BA);
+const lightPurple = Color(0xFFFFAEFF);
+const lightGreen = Color(0xFF31BEAF);
+const lightGrey = Color(0xFFD4D4D4);
+const mediumGreen = Color(0xFF669C82);
+const lightPaper = Color(0xFFFFFEF5);
+const lightWhite = Color(0xFFE8FFF3);
+const darkBlue = Color(0xFF1D1D35);
+const lightBlue = Color(0xFF006DCF);
+const errorColor = Color(0xFFB52000);
